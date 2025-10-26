0c0376d0edf089950cddd380976de4724b4cfed7
codewithrafey <rafeyrashid0@gmail.com>
2025-10-26 13:04:54 +0500

    new-change

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,41 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Internship from './pages/Internship'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import InternshipDetails from './pages/InternshipDetails'
+import { useEffect } from "react";
 
-function App() {
+function ScrollToTop() {
+  const { pathname } = useLocation();
 
-  return (
-    <>
-    <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path='/'  element={ <Home /> }/>
-      <Route path='/internship'  element={ <Internship /> }/>
-      <Route path='/internship/:id'  element={ <InternshipDetails /> }/>
-      <Route path='/about'  element={ <About /> }/>
-      <Route path='/contact'  element={ <Contact /> }/>
-    </Routes>
-    <Footer />
-    </BrowserRouter>
-    </>
-  )
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+
+  return null;
 }
 
-export default App
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/internship' element={<Internship />} />
+          <Route path='/internship/:id' element={<InternshipDetails />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
